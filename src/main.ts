30b7fbd7aed998e72704e1e1b52e020ffaaa89b1
Cristian F. Tovar <cristian00tovar@gmail.com>
2024-01-13 18:49:38 -0500

    🙏 (2:02:28) clean refactor using a utils.ts file:
    I had to comment out the determine_winner call because it puts TypeScript
    mad

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import "./style.css";
 import { Sprite, Fighter } from "@/js/classes";
 import { canvas, c, gravity } from "@/js/canvas";
+import { rectangular_collision, determine_winner, decrease_timer } from "@/js/utils";
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
@@ -40,51 +41,8 @@ const keys = {
 };
 
 let last_key: string = "";
+let timer_id: number = 0;
 
-const rectangular_collision = ({ attacker, target, }: { attacker: Fighter; target: Fighter; }) => {
-  const attackerCollidesTargetX = attacker.attack_box.position.x + attacker.attack_box.width >= target.position.x;
-  const targetCollidesAttackerX = attacker.attack_box.position.x <= target.position.x + target.width;
-  const attackerCollidesTargetY = attacker.attack_box.position.y + attacker.attack_box.height >= target.position.y;
-  const targetCollidesAttackerY = attacker.attack_box.position.y <= target.position.y + target.height;
-
-  return (
-    attackerCollidesTargetX &&
-    targetCollidesAttackerX &&
-    attackerCollidesTargetY &&
-    targetCollidesAttackerY &&
-    attacker.is_attacking
-  );
-};
-
-let timer = 60;
-let timer_id: number;
-
-const determine_winner = ({player, enemy, timer_id}: {player: Fighter; enemy: Fighter; timer_id: number})=> {
-  clearTimeout(timer_id);
-  if(player.health === enemy.health){
-    document.querySelector<HTMLDivElement>("#sign")!.innerHTML = "Tie";
-  } else if (player.health > enemy.health){
-    document.querySelector<HTMLDivElement>("#sign")!.innerHTML = "Enemy wins";
-  } else if (player.health < enemy.health){
-    document.querySelector<HTMLDivElement>("#sign")!.innerHTML = "Player wins";
-  }
-
-  document.querySelector<HTMLDivElement>("#sign")!.style.display = "flex";
-}
-
-const decrease_timer = () => {
-  if(timer > 0){
-    timer_id = setTimeout(decrease_timer, 1000);
-    timer--;
-    document.querySelector<HTMLDivElement>("#timer")!.innerHTML = timer.toString();
-  }
-
-  if(timer === 0) {
-    document.querySelector<HTMLDivElement>("#sign")!.style.display = "flex";
-    determine_winner({player, enemy, timer_id});
-  }
-  
-}
 
 decrease_timer();
 
